442e86b93d52cdb1d08d4f1c78cab56063cbaf6d
Xander P. <ajamespickering@gmail.com>
2026-03-03 12:06:17 -0700

    Initial svelte setup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<div
+    class="bg-blue-300 w-screen h-screen flex flex-col items-center justify-between gap-y-8 max-w-3xl m-auto p-8"
+>
+    <div class="text-4xl bg-blue-400 w-full p-4 text-center">
+        <h3 class="text-sm">THE</h3>
+        <h1>Two-Sentence Daily</h1>
+    </div>
+
+    <div class="w-full flex flex-col gap-y-4">
+        <div class="text-center bg-blue-500 p-4">
+            <h2>Write a two-sentence [type] story about:</h2>
+            <ul class="flex gap-x-16 font-semibold justify-center">
+                <li>Keyword 1</li>
+                <li>Keyword 2</li>
+                <li>Keyword 3</li>
+            </ul>
+        </div>
+
+        <div class="flex flex-col gap-y-2 w-full p-8 bg-blue-400">
+            <div class="w-full">
+                <h1 class="text-center font-bold">Sentence 1:</h1>
+                <input class="border w-full" />
+            </div>
+
+            <div class="w-full">
+                <h1 class="text-center font-bold">Sentence 2:</h1>
+                <input class="border w-full" />
+            </div>
+        </div>
+    </div>
+
+    <div class="bg-blue-400 w-full text-center p-4">
+        <h1><a href="https://github.com/xandwr">@xandwr</a> 2026</h1>
+    </div>
+</div>
